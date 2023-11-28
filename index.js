22d86177a20036c73c42697fd29acdcf1d7b7934
mounikamaddi <123763049+mounikamaddi@users.noreply.github.com>
2023-11-27 20:31:58 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+// server.js or index.js (at the project root or inside an `/api` directory)
+//http://localhost:3000/cs212/homework/8
+const express = require('express');
+const path = require('path'); // Include the path module
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+// Serve index.html at the root path
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Your existing route for /cs212/homework/8
+app.get('/cs212/homework/8', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Your POST route for /cs212/homework/8
+app.post('/cs212/homework/8', (req, res) => {
+    const { adjective, pluralNoun, personName, verb, noun } = req.body;
+    const madLib = `Today, I saw a ${adjective} ${pluralNoun} which made me think of ${personName}. 
+                    Then I decided to ${verb} by the ${noun}.`;
+
+    res.send(madLib);
+});
+
+module.exports = app;
